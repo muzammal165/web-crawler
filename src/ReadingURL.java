b11ef990aa5ef6b9388147bb5c402102a831e722
Muzammal Hussain <muzammal.hussain@campus.tu-berlin.de>
2019-04-07 16:41:22 +0200

    Basic Structure

---
+++ src/ReadingURL.java
@@ -0,0 +1,17 @@
+//import java.net.*;
+//import java.io.*;
+//public class ReadingURL {
+//
+//	public static void main(String[] args)  {
+//		// TODO Auto-generated method stub
+//		URL oracle = new URL("http://www.oracle.com/");
+//        URLConnection yc = oracle.openConnection();
+//        BufferedReader in = new BufferedReader(new InputStreamReader(
+//                                    yc.getInputStream()));
+//        String inputLine;
+//        while ((inputLine = in.readLine()) != null) 
+//            System.out.println(inputLine);
+//        in.close();
+//	}
+//
+//}
---
+++ src/mine.java
@@ -0,0 +1,11 @@
+
+public class mine {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.print("Muzammal Hussain");
+		
+	}
+
+}
