5844748159ee04480b9418b9fef99d437daf9899
Muzammal Hussain <muzammal.hussain@campus.tu-berlin.de>
2019-04-07 19:21:09 +0200

    Complete

--- src/ReadingURL.java
+++ src/ReadingURL.java
@@ -1,17 +1,23 @@
-//import java.net.*;
-//import java.io.*;
-//public class ReadingURL {
-//
-//	public static void main(String[] args)  {
-//		// TODO Auto-generated method stub
-//		URL oracle = new URL("http://www.oracle.com/");
-//        URLConnection yc = oracle.openConnection();
-//        BufferedReader in = new BufferedReader(new InputStreamReader(
-//                                    yc.getInputStream()));
-//        String inputLine;
-//        while ((inputLine = in.readLine()) != null) 
-//            System.out.println(inputLine);
-//        in.close();
-//	}
-//
-//}
+import java.net.*;
+import java.io.*;
+public class ReadingURL {
+
+	public String getHTML(String url) throws IOException  {
+
+		URL urlObj = new URL(url);
+        URLConnection urlConnection = urlObj.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                                    urlConnection.getInputStream()));
+   
+        String html = "";
+        String inputLine;
+      
+        while ((inputLine = in.readLine()) != null)
+        {
+        	html = html + inputLine;
+        }
+        in.close();
+        return html;
+	}
+
+}
--- src/mine.java
+++ src/mine.java
@@ -1,11 +1,50 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class mine {
 
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.print("Muzammal Hussain");
-		
+		ArrayList<String> links = new ArrayList<String>();
+		int counter = 0;
+		String mainurl = "https://www.visual-meta.com/";
+		links.add(mainurl);
+		try {
+			while(links.size() <= 999)
+			{
+				String currenturl = links.get(counter);
+				System.out.println("===============");
+				System.out.println("current url : " + currenturl);
+				System.out.println("===============");
+				
+				Document doc = Jsoup.connect(currenturl).get();
+				Elements elements = doc.select("a");
+				int size = elements.size();
+				
+				for(int i=1 ; i<size ; i++)
+				{
+					String link = elements.get(i).attr("href");
+					
+					if ( link.length() > 4)
+					{
+						if (!link.startsWith("http"))
+						{
+							link = mainurl + link;
+						}
+						links.add(link);
+						System.out.println(link);
+					}		
+				}
+				counter++;
+			}
+			System.out.println("\n1000 Urls rerieved.");
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("invalid url");
+		}
 	}
-
 }
